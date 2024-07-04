f2d8c912cf577e599dc5e74a1e2a78d189811821
progetto65 <2412484787@qq.com>
2024-07-04 16:20:02 +0800

    nothing but a test

--- src/views/Category.jsx
+++ src/views/Category.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useHistory } from "react-router-dom";
 
 import { NavBar, Space, Toast, Tabs, Badge } from "antd-mobile";
@@ -9,10 +9,17 @@ import "./Category.scss";
 
 export default () => {
   const history = useHistory();
-  
+
   const [category, changeCg] = useState([]);
-  const [TabName,setTabName] = useState(["手机","手机充电","手机配件","音乐设备","智能硬件","游戏摄影","日用出行"])
-  
+  const [TabName, setTabName] = useState([
+    "手机",
+    "手机充电",
+    "手机配件",
+    "音乐设备",
+    "智能硬件",
+    "游戏摄影",
+    "日用出行",
+  ]);
 
   const doSearch = async (val) => {
     const { data } = await request.get("/category/sort", {
@@ -50,38 +57,48 @@ export default () => {
       </NavBar>
 
       {/* 标签页 */}
-      <Tabs defaultActiveKey={TabName[0]} onChange={(e)=>{doSearch(e)}}>
-        {TabName.map((oneTab,index)=>{
+      <Tabs
+        defaultActiveKey={TabName[0]}
+        onChange={(e) => {
+          doSearch(e);
+        }}
+      >
+        {TabName.map((oneTab, index) => {
           return (
-            <Tabs.Tab title={TabName[index]} key={TabName[index]} >
-
-               <div className="hot">
-                    {/* 标题 */}
-                    <div className="hot-title">
-                        <span className="phonetitle">{oneTab}</span> 
-                    </div>
-
-                    {/* 商品 */}
-                    <div className="hot-list">
-                        {
-                            category.map(item=>{
-                                return (
-                                    <div className="hot-item" key={item.skuID} onClick={()=>{history.push('/details/'+item.skuID)}}>
-                                        <img src={require("../assets/images/"+item.images).default} />
-                                        <h3>{item.skuName}</h3>
-                                        <p className="promo">{item.brief}</p>
-                                        <p className="item-price">{item.salePrice}</p>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
+            <Tabs.Tab title={TabName[index]} key={TabName[index]}>
+              <div className="hot">
+                {/* 标题 */}
+                <div className="hot-title">
+                  <span className="phonetitle">{oneTab}</span>
                 </div>
+
+                {/* 商品 */}
+                <div className="hot-list">
+                  {category.map((item) => {
+                    return (
+                      <div
+                        className="hot-item"
+                        key={item.skuID}
+                        onClick={() => {
+                          history.push("/details/" + item.skuID);
+                        }}
+                      >
+                        <img
+                          src={
+                            require("../assets/images/" + item.images).default
+                          }
+                        />
+                        <h3>{item.skuName}</h3>
+                        <p className="promo">{item.brief}</p>
+                        <p className="item-price">{item.salePrice}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
             </Tabs.Tab>
-          )
-        })
-        }
-        
+          );
+        })}
       </Tabs>
     </>
   );
